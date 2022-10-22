e38dc2f6130c9634fcaa05c6e9382d25a4f56432
naoki-funawatari <g011c1180@gmail.com>
2022-10-22 14:05:35 +0900

    feat: 試しにヘッダー部分を分割。

---
+++ src/features/todolits/TodoList.vue
@@ -0,0 +1,172 @@
+<script setup lang="ts">
+import { ref, computed, onBeforeMount } from "vue"
+import WeightSelector from "../../components/WeightSelector.vue"
+import RangeInput from "../../components/RangeInput.vue"
+import { WeightTypes } from "../../types"
+import type { Todo } from "../../types"
+import { todoDefaults } from "../../constants"
+import TodoListHeader from "./TodoListHeader.vue"
+
+const list = ref<Todo[]>([])
+
+onBeforeMount(() => {
+  const progress = localStorage.getItem("progress")
+  list.value = progress ? JSON.parse(progress) as Todo[] : todoDefaults
+})
+
+const addList = (text: string): void => {
+  const id = Math.max(...list.value.map(o => o.id)) + 1
+  const order = Math.max(...list.value.map(o => o.order)) + 1
+  const item: Todo = { id, text, progress: 0, weight: WeightTypes.Low, order }
+  list.value = [item, ...list.value]
+}
+const deleteItem = (id: number) => {
+  list.value = list.value.filter(o => o.id !== id)
+}
+const changeOrder = (type: "up" | "down", currentOrder: number) => {
+  let ordered = [...list.value]
+  const index = ordered.findIndex(o => o.order === currentOrder)
+  ordered[index].order += (type === "up" ? -1.5 : 1.5)
+  ordered.sort((a, b) => a.order - b.order)
+  ordered.forEach((o, i) => o.order = i + 1)
+  list.value = ordered
+}
+const registerProgress = () => {
+  const value = JSON.stringify(list.value)
+  localStorage.setItem("progress", value)
+}
+
+const proration = (progress: number, weight: number, trunc: boolean = false) => {
+  const totalWeight = list.value.reduce((a, b) => a + Number(b.weight), 0)
+  const ratio = weight / totalWeight
+  return trunc ? Math.trunc(progress * ratio) : progress * ratio
+}
+
+const overallProgress = computed(() => {
+  const values = list.value.map(o => proration(o.progress, o.weight))
+  const total = values.reduce((a, b) => a + b, 0)
+  return Math.trunc(total)
+})
+const sorted = computed(() => list.value.sort((a, b) => a.order - b.order))
+</script>
+
+<template>
+  <TodoListHeader :addList="addList" />
+  <hr>
+  <div class="item">
+    <div class="text">内容</div>
+    <div class="progress-bar">@</div>
+    <div class="progress">@</div>
+    <div class="weight">@</div>
+    <div class="proration">@</div>
+    <div class="delete">@</div>
+    <div class="sorting">@</div>
+    <div class="sorting">@</div>
+  </div>
+  <div v-for="item in sorted" class="item">
+    <div class="text">{{item.text}}</div>
+    <div class="progress-bar">
+      <RangeInput v-model="item.progress" />
+    </div>
+    <div class="progress">
+      <span>{{item.progress}} %</span>
+    </div>
+    <div class="weight">
+      <WeightSelector v-model="item.weight" />
+    </div>
+    <div class="proration">{{proration(item.progress, item.weight, true)}} %</div>
+    <div class="delete" @click="deleteItem(item.id)">
+      <button>削除</button>
+    </div>
+    <div class="sorting">
+      <button v-if="item.order!==1" @click="changeOrder('up', item.order)">👆</button>
+    </div>
+    <div class="sorting">
+      <button v-if="item.order!==list.length" @click="changeOrder('down', item.order)">👇</button>
+    </div>
+  </div>
+  <hr>
+  <div class="item">
+    <div class="text">進捗率</div>
+    <div class="progress-bar">
+      <progress max="100" :value="overallProgress"></progress>
+    </div>
+    <div class="progress">@</div>
+    <div class="weight">@</div>
+    <div class="proration">{{overallProgress}} %</div>
+    <div class="delete">@</div>
+    <div class="sorting">@</div>
+    <div class="sorting">@</div>
+  </div>
+  <hr>
+  <div class="item">
+    <div class="text">@</div>
+    <div class="progress-bar">@</div>
+    <div class="progress">@</div>
+    <div class="weight">@</div>
+    <div class="proration">@</div>
+    <div class="delete">
+      <button @click="registerProgress">保存</button>
+    </div>
+    <div class="sorting">@</div>
+    <div class="sorting">@</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.item ::v-deep {
+  display: flex;
+  flex-direction: row;
+  column-gap: 10px;
+  width: 800px;
+  margin-bottom: 5px;
+
+  .text {
+    flex-grow: 1;
+    border-bottom: 1px solid black
+  }
+
+  .progress-bar {
+    width: 160px;
+    text-align: right;
+    border-bottom: 1px solid black
+  }
+
+  .progress {
+    width: 60px;
+    text-align: right;
+    border-bottom: 1px solid black
+  }
+
+  .weight {
+    width: 60px;
+    text-align: right;
+    border-bottom: 1px solid black
+  }
+
+  .proration {
+    width: 60px;
+    text-align: right;
+    border-bottom: 1px solid black
+  }
+
+  .delete {
+    width: 50px;
+    text-align: right;
+  }
+
+  .sorting {
+    width: 30px;
+    text-align: right;
+  }
+}
+
+input[type=range],
+progress {
+  width: 150px;
+}
+
+.noborder {
+  border: none;
+}
+</style>
---
+++ src/features/todolits/TodoListHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref, PropType } from "vue"
+import TextInput from "../../components/TextInput.vue"
+
+const { addList } = defineProps({
+  addList: {
+    type: Function as PropType<(text: string) => void>,
+    required: true
+  }
+})
+
+const input = ref<string>("")
+
+const handleAddClick = () => {
+  if (input.value === "") {
+    return
+  }
+  addList(input.value)
+  input.value = ""
+}   
+</script>
+
+<template>
+  <div class="item">
+    <div class="text">
+      <TextInput type="text" v-model="input" />
+    </div>
+    <div class="progress-bar">@</div>
+    <div class="progress">@</div>
+    <div class="weight">@</div>
+    <div class="proration">@</div>
+    <div class="delete"><button @click="handleAddClick">追加</button></div>
+    <div class="sorting">@</div>
+    <div class="sorting">@</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
